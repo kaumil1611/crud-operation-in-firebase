57c292406b2ba08e6f7683ad8664ab2799ecb9fb
indianic <indianic@indianics-iMac-Pro-4.local>
2021-10-07 12:11:07 +0530

    firebase-config added

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,15 +2,6 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDeOhvss8lc5wRgZrEXyBD6YCx_pNleO3E",
-//   authDomain: "react-http-88cf0.firebaseapp.com",
-//   databaseURL: "https://react-http-88cf0-default-rtdb.firebaseio.com",
-//   projectId: "react-http-88cf0",
-//   storageBucket: "react-http-88cf0.appspot.com",
-//   messagingSenderId: "958328592550",
-//   appId: "1:958328592550:web:fdf1abf332308d8dad7c67",
-// };
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -25,24 +16,4 @@ firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 
-//const auth = firebase.auth();
-
-// console.log(auth);
-
 export { db };
-
-// firebase.initializeApp(firebaseConfig);
-
-// const db = firebase.firestore();
-
-// const auth = firebase.auth();
-
-// console.log(auth);
-
-// export { db };
-
-// firebase.initializeApp(firebaseConfig);
-
-// const auth = firebase.auth();
-
-// export { auth };
